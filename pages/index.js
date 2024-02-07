b1794f76a3a5f249447cf1640fa07363fe9c82e5
ScofieldaCreep <tju_scofield@outlook.com>
2024-02-07 03:23:25 -0500

    Add useState hook and new component CompB

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 /* 
 index.js
 用途: index.js通常位于pages目录下，它代表的是应用的入口页面，也就是访问应用根URL时首先加载的页面。
@@ -6,21 +7,31 @@ index.js
 特定页面: 与_app.js不同，index.js只关心自己的布局和逻辑，不会影响到其他页面。
 */
 function CompA() {
-  return <h1>CompA</h1>;
+  React.createElement("h1", null, "CompA");
 }
-
+function CompB() {
+  return <h1>CompB</h1>;
+}
+// JSX 语法: 用于在 JavaScript 代码中直接编写 HTML 标签 -> Javascript XML
 class CompC extends React.Component {
   render() {
-    return <h1>CompC</h1>;
+    return React.createElement("h1", null, "Hello CompC!");
   }
 }
 
 function Home() {
+  // useState: 用于在函数组件中声明状态变量
+  // [stateValue, mutateStateFunction] = useState(initialState)
+  const [myValue, setValue] = useState(10);
+  const increVal = (incrementor) => {
+    setValue(myValue + incrementor);
+  };
   return (
     <div>
-      <h1>Hello World</h1>
-      <CompA></CompA>
-      <CompC></CompC>
+      Current Value: <h1>{myValue}</h1>
+      <button onClick={() => setValue(myValue + 1)}>+</button>
+      <button onClick={() => setValue(myValue - 1)}>-</button>
+      <CompA />
     </div>
   );
 }
